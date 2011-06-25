73cf44c5890b0857db0d8a3ab0db4caa47c86ab9
thomasjungblut <thomas.jungblut@gmail.com>
2011-06-25 21:18:01 +0200

    improved the hashtable

--- src/de/jungblut/datastructure/HashTable.java
+++ src/de/jungblut/datastructure/HashTable.java
@@ -2,9 +2,9 @@ package de.jungblut.datastructure;
 
 import java.util.LinkedList;
 
-public class HashTable {
+public final class HashTable<K, V> {
 
-	private LinkedList<String>[] entries;
+	private LinkedList<Entry<K, V>>[] entries;
 	private double loadFactor = 0.75;
 	private int initialSize = 10;
 	private int size = 0;
@@ -12,23 +12,43 @@ public class HashTable {
 
 	@SuppressWarnings("unchecked")
 	public HashTable() {
-		entries = (LinkedList<String>[]) new LinkedList[initialSize];
+		entries = (LinkedList<Entry<K, V>>[]) new LinkedList[initialSize];
 		threshold = (int) (loadFactor * entries.length);
 	}
 
-	public void add(String key) {
-		addInternal(key, entries);
+	public void put(K key, V value) {
+		addInternal(new Entry<K, V>(key, value), entries);
 	}
 
-	private void addInternal(String key, LinkedList<String>[] entries) {
-		int bucked = getBuckedIndexFor(key.hashCode(), entries.length);
-
-		if (entries[bucked] == null) {
-			entries[bucked] = new LinkedList<String>();
+	public V get(K key) {
+		int bucket = getBucketIndexFor(key.hashCode(), entries.length);
+		LinkedList<Entry<K, V>> list = entries[bucket];
+		if (list == null) {
+			return null;
+		} else {
+			for (Entry<K, V> entry : list) {
+				if (entry.getKey().equals(key))
+					return entry.getValue();
+			}
 		}
-		if (!entries[bucked].contains(key)) {
-			entries[bucked].add(key);
+		return null;
+	}
+
+	private void addInternal(Entry<K, V> entry,
+			LinkedList<Entry<K, V>>[] entryArray) {
+		int bucket = getBucketIndexFor(entry.getKey().hashCode(),
+				entryArray.length);
+		if (entryArray[bucket] == null) {
+			entryArray[bucket] = new LinkedList<Entry<K, V>>();
+		}
+
+		int index = getIndexForKey(entry.getKey(), entryArray[bucket]);
+
+		if (index == -1) {
+			entryArray[bucket].add(entry);
 			size++;
+		} else {
+			entryArray[bucket].set(index, entry);
 		}
 
 		if (size > threshold) {
@@ -36,66 +56,125 @@ public class HashTable {
 		}
 	}
 
+	private int getIndexForKey(K key, LinkedList<Entry<K, V>> list) {
+		int count = -1;
+		for (Entry<K, V> element : list) {
+			count++;
+			if (element.getKey().equals(key)){
+				return count;
+			}
+		}
+		return -1;
+	}
+
 	@SuppressWarnings("unchecked")
 	private void rehash() {
 		// we're just doubling the size available now
-		LinkedList<String>[] temp = (LinkedList<String>[]) new LinkedList[entries.length * 2];
+		LinkedList<Entry<K, V>>[] temp = (LinkedList<Entry<K, V>>[]) new LinkedList[entries.length * 2];
 		threshold = (int) (loadFactor * temp.length);
 		size = 0;
-		for (LinkedList<String> list : entries) {
+		for (LinkedList<Entry<K, V>> list : entries) {
 			if (list != null) {
-				for (String s : list) {
-					addInternal(s, temp);
+				for (Entry<K, V> entry : list) {
+					addInternal(entry, temp);
 				}
 			}
 		}
 		entries = temp;
 	}
 
-	public boolean contains(String key) {
-		int bucked = getBuckedIndexFor(key.hashCode(), entries.length);
-		if (entries[bucked] != null) {
-			for (String s : entries[bucked]) {
-				if (key.equals(s))
+	public boolean contains(K key) {
+		int bucket = getBucketIndexFor(key.hashCode(), entries.length);
+		if (entries[bucket] != null) {
+			for (Entry<K, V> s : entries[bucket]) {
+				if (key.equals(s.getKey()))
 					return true;
 			}
 		}
 		return false;
 	}
 
-	private int getBuckedIndexFor(int hash, int size) {
+	private int getBucketIndexFor(int hash, int size) {
 		return Math.abs(hash % (size - 1));
 	}
 
+	private static final class Entry<K, V> {
+		private final K k;
+		private final V v;
+
+		public Entry(K k, V v) {
+			super();
+			this.k = k;
+			this.v = v;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((k == null) ? 0 : k.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			@SuppressWarnings("rawtypes")
+			Entry other = (Entry) obj;
+			if (k == null) {
+				if (other.k != null)
+					return false;
+			} else if (!k.equals(other.k))
+				return false;
+			return true;
+		}
+
+		public K getKey() {
+			return k;
+		}
+
+		public V getValue() {
+			return v;
+		}
+	}
+
 	public static void main(String[] args) {
-		HashTable table = new HashTable();
-		table.add("ABCDEa123abc");
+		HashTable<String, String> table = new HashTable<String, String>();
+		table.put("ABCDEa123abc", "1");
 		System.out.println(table.contains("ABCDEa123abc"));
-		table.add("ABCDFB123abc");
+		table.put("ABCDFB123abc", "2");
 		System.out.println(table.contains("ABCDFB123abc"));
-		table.add("ABC");
+		table.put("ABC", "ABC");
 		System.out.println(table.contains("ABC"));
-		table.add("CAB");
+		table.put("CAB", "CAB");
 		System.out.println(table.contains("CAB"));
-		table.add("BD");
+		table.put("BD", "BD");
 		System.out.println(table.contains("BD"));
+
 		// testing the rehashing
+		table.put("ASDASDA", "ASDASDA");
+		table.put("ASDQWEAGGAS", "ASDASDA");
+		table.put("ASDHASLÖKA", "ASDASDA");
+		table.put("ASKZHO", "ASDASDA");
+		table.put("kfgkgk", "ASDASDA");
+		table.put("jdfjsj", "ASDASDA");
 
-		table.add("ASDASDA");
-		table.add("ASDQWEAGGAS");
-		table.add("ASDHASLÖKA");
-		table.add("ASKZHO");
-		table.add("kfgkgk");
-		table.add("jdfjsj");
-
-		table.add("asdffdsferahhhh");
-		table.add("jkasjkaasdf");
-		table.add("adsjklöajka");
-		table.add("ASsdjksjksjkKZHO");
-		table.add("fgajknanaöjk");
+		table.put("asdffdsferahhhh", "ASDASDA");
+		table.put("jkasjkaasdf", "ASDASDA");
+		table.put("adsjklöajka", "asd");
+		table.put("ASsdjksjksjkKZHO", "ASDASDA");
+		table.put("fgajknanaöjk", "ASDASDA");
 		System.out.println(table.contains("fgajknanaöjk"));
+		System.out.println(table.get("adsjklöajka"));
 		System.out.println(table.contains("CAB"));
-		table.add("jdfjshhhhhhhhj");
+		table.put("jdfjshhhhhhhhj", "ASDASDA");
+
+		System.out.println(table.get("fgajknanaöjk"));
 
 	}
 
