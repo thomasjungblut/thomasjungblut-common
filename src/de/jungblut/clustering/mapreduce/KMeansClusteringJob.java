057b556dff79f7bac1e68f7b646839026e77d0ba
thomasjungblut <thomas.jungblut@gmail.com>
2011-12-03 16:10:50 +0100

    added my old mapreduce implementation of k-means

---
+++ src/de/jungblut/clustering/mapreduce/KMeansClusteringJob.java
@@ -0,0 +1,145 @@
+package de.jungblut.clustering.mapreduce;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import de.jungblut.clustering.model.ClusterCenter;
+import de.jungblut.clustering.model.Vector;
+
+public class KMeansClusteringJob {
+
+    private static final Log LOG = LogFactory.getLog(KMeansClusteringJob.class);
+
+    public static void main(String[] args) throws IOException,
+	    InterruptedException, ClassNotFoundException {
+
+	int iteration = 1;
+	Configuration conf = new Configuration();
+	conf.set("num.iteration", iteration + "");
+
+	Path in = new Path("files/clustering/import/data");
+	Path center = new Path("files/clustering/import/center/cen.seq");
+	conf.set("centroid.path", center.toString());
+	Path out = new Path("files/clustering/depth_1");
+
+	Job job = new Job(conf);
+	job.setJobName("KMeans Clustering");
+
+	job.setMapperClass(KMeansMapper.class);
+	job.setReducerClass(KMeansReducer.class);
+	job.setJarByClass(KMeansMapper.class);
+
+	SequenceFileInputFormat.addInputPath(job, in);
+	FileSystem fs = FileSystem.get(conf);
+	if (fs.exists(out))
+	    fs.delete(out, true);
+
+	if (fs.exists(center))
+	    fs.delete(out, true);
+
+	if (fs.exists(in))
+	    fs.delete(out, true);
+
+	final SequenceFile.Writer centerWriter = SequenceFile.createWriter(fs,
+		conf, center, ClusterCenter.class, IntWritable.class);
+	final IntWritable value = new IntWritable(0);
+	centerWriter.append(new ClusterCenter(new Vector(1, 1)), value);
+	centerWriter.append(new ClusterCenter(new Vector(5, 5)), value);
+	centerWriter.close();
+
+	final SequenceFile.Writer dataWriter = SequenceFile.createWriter(fs,
+		conf, in, ClusterCenter.class, Vector.class);
+	dataWriter
+		.append(new ClusterCenter(new Vector(0, 0)), new Vector(1, 2));
+	dataWriter.append(new ClusterCenter(new Vector(0, 0)),
+		new Vector(16, 3));
+	dataWriter
+		.append(new ClusterCenter(new Vector(0, 0)), new Vector(3, 3));
+	dataWriter
+		.append(new ClusterCenter(new Vector(0, 0)), new Vector(2, 2));
+	dataWriter
+		.append(new ClusterCenter(new Vector(0, 0)), new Vector(2, 3));
+	dataWriter.append(new ClusterCenter(new Vector(0, 0)),
+		new Vector(25, 1));
+	dataWriter
+		.append(new ClusterCenter(new Vector(0, 0)), new Vector(7, 6));
+	dataWriter
+		.append(new ClusterCenter(new Vector(0, 0)), new Vector(6, 5));
+	dataWriter.append(new ClusterCenter(new Vector(0, 0)), new Vector(-1,
+		-23));
+	dataWriter.close();
+
+	SequenceFileOutputFormat.setOutputPath(job, out);
+	job.setInputFormatClass(SequenceFileInputFormat.class);
+	job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+	job.setOutputKeyClass(ClusterCenter.class);
+	job.setOutputValueClass(Vector.class);
+
+	job.waitForCompletion(true);
+
+	long counter = job.getCounters()
+		.findCounter(KMeansReducer.Counter.CONVERGED).getValue();
+	iteration++;
+	while (counter > 0) {
+	    conf = new Configuration();
+	    conf.set("centroid.path", center.toString());
+	    conf.set("num.iteration", iteration + "");
+	    job = new Job(conf);
+	    job.setJobName("KMeans Clustering " + iteration);
+
+	    job.setMapperClass(KMeansMapper.class);
+	    job.setReducerClass(KMeansReducer.class);
+	    job.setJarByClass(KMeansMapper.class);
+
+	    in = new Path("files/clustering/depth_" + (iteration - 1) + "/");
+	    out = new Path("files/clustering/depth_" + iteration);
+
+	    SequenceFileInputFormat.addInputPath(job, in);
+	    if (fs.exists(out))
+		fs.delete(out, true);
+
+	    SequenceFileOutputFormat.setOutputPath(job, out);
+	    job.setInputFormatClass(SequenceFileInputFormat.class);
+	    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+	    job.setOutputKeyClass(ClusterCenter.class);
+	    job.setOutputValueClass(Vector.class);
+
+	    job.waitForCompletion(true);
+	    iteration++;
+	    counter = job.getCounters()
+		    .findCounter(KMeansReducer.Counter.CONVERGED).getValue();
+	}
+
+	Path result = new Path("files/clustering/depth_" + (iteration - 1)
+		+ "/");
+
+	FileStatus[] stati = fs.listStatus(result);
+	for (FileStatus status : stati) {
+	    if (!status.isDir()) {
+		Path path = status.getPath();
+		LOG.info("FOUND " + path.toString());
+		SequenceFile.Reader reader = new SequenceFile.Reader(fs, path,
+			conf);
+		ClusterCenter key = new ClusterCenter();
+		Vector v = new Vector();
+		while (reader.next(key, v)) {
+		    LOG.info(key + " / " + v);
+		}
+		reader.close();
+	    }
+	}
+    }
+
+}
---
+++ src/de/jungblut/clustering/mapreduce/KMeansMapper.java
@@ -0,0 +1,63 @@
+package de.jungblut.clustering.mapreduce;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import de.jungblut.clustering.model.ClusterCenter;
+import de.jungblut.clustering.model.DistanceMeasurer;
+import de.jungblut.clustering.model.Vector;
+
+// first iteration, k-random centers, in every follow-up iteration we have new calculated centers
+public class KMeansMapper extends
+	Mapper<ClusterCenter, Vector, ClusterCenter, Vector> {
+
+    List<ClusterCenter> centers = new LinkedList<ClusterCenter>();
+
+    @Override
+    protected void setup(Context context) throws IOException,
+	    InterruptedException {
+	super.setup(context);
+	Configuration conf = context.getConfiguration();
+	Path centroids = new Path(conf.get("centroid.path"));
+	FileSystem fs = FileSystem.get(conf);
+
+	SequenceFile.Reader reader = new SequenceFile.Reader(fs, centroids,
+		conf);
+	ClusterCenter key = new ClusterCenter();
+	IntWritable value = new IntWritable();
+	while (reader.next(key, value)) {
+	    centers.add(new ClusterCenter(key));
+	}
+	reader.close();
+    }
+
+    @Override
+    protected void map(ClusterCenter key, Vector value, Context context)
+	    throws IOException, InterruptedException {
+
+	ClusterCenter nearest = null;
+	double nearestDistance = Double.MAX_VALUE;
+	for (ClusterCenter c : centers) {
+	    double dist = DistanceMeasurer.measureDistance(c, value);
+	    if (nearest == null) {
+		nearest = c;
+		nearestDistance = dist;
+	    } else {
+		if (nearestDistance > dist) {
+		    nearest = c;
+		    nearestDistance = dist;
+		}
+	    }
+	}
+	context.write(nearest, value);
+    }
+
+}
