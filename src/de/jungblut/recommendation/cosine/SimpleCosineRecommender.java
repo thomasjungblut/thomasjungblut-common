a0655aac8e493bea5c4dbe12bb0707d9c2dd4269
thomasjungblut <thomas.jungblut@gmail.com>
2012-03-10 16:54:22 +0100

    compare

--- src/de/jungblut/recommendation/cosine/SimpleCosineRecommender.java
+++ src/de/jungblut/recommendation/cosine/SimpleCosineRecommender.java
@@ -132,7 +132,7 @@ public class SimpleCosineRecommender {
           Collections.reverseOrder(new Comparator<Double>() {
             @Override
             public int compare(Double o1, Double o2) {
-              return Double.compare(o1, o2);
+              return Double.compare(o2, o1);
             }
           }));
 
