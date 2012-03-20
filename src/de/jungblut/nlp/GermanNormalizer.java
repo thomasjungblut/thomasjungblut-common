ef1550714a272272f866f0288c66b89e5446d0ec
thomasjungblut <thomas.jungblut@gmail.com>
2012-03-20 16:34:04 +0100

    unicode

--- src/de/jungblut/nlp/GermanNormalizer.java
+++ src/de/jungblut/nlp/GermanNormalizer.java
@@ -22,15 +22,15 @@ public class GermanNormalizer implements Normalizer {
 
   public final static HashSet<String> GERMAN_STOP_WORDS = new HashSet<String>(
       Arrays.asList(new String[] { "and", "the", "of", "to", "einer", "eine",
-          "eines", "einem", "einen", "der", "die", "das", "dass", "daß", "du",
+          "eines", "einem", "einen", "der", "die", "das", "dass", "da\u00DF", "du",
           "er", "sie", "es", "was", "wer", "wie", "wir", "und", "oder", "ohne",
           "mit", "am", "im", "in", "aus", "auf", "ist", "sein", "war", "wird",
-          "ihr", "ihre", "ihres", "ihnen", "ihrer", "als", "für", "von", "mit",
+          "ihr", "ihre", "ihres", "ihnen", "ihrer", "als", "f\u00FCr", "von", "mit",
           "dich", "dir", "mich", "mir", "mein", "sein", "kein", "durch",
           "wegen", "wird", "sich", "bei", "beim", "noch", "den", "dem", "zu",
           "zur", "zum", "auf", "ein", "auch", "werden", "an", "des", "sein",
           "sind", "vor", "nicht", "sehr", "um", "unsere", "ohne", "so", "da",
-          "nur", "diese", "dieser", "diesem", "dieses", "nach", "über", "mehr",
+          "nur", "diese", "dieser", "diesem", "dieses", "nach", "\u00FCber", "mehr",
           "hat", "bis", "uns", "unser", "unserer", "unserem", "unsers", "euch",
           "euers", "euer", "eurem", "ihr", "ihres", "ihrer", "ihrem", "alle",
           "vom" }));
