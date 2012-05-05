ca47eff455560990b3caddd1209765d553c99c8d
thomasjungblut <thomas.jungblut@gmail.com>
2012-05-06 00:06:02 +0200

    I have a plan...

---
+++ src/de/jungblut/clustering/DBScan.java
@@ -0,0 +1,19 @@
+package de.jungblut.clustering;
+
+public class DBScan {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    /**
+     * PLAN: 1. compute distance matrix between the points <br/>
+     * 2. extract adjacent points via threshold epsilon and minpoints s <br/>
+     * 3. run some kind of SSSP <br/>
+     * 4. decouple clusters <br/>
+     * 5. ??? <br/>
+     * 6. PROFIT!
+     */
+  }
+
+}
