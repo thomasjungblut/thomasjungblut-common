17680ef911e1e0e44358090e7bc79d95b814b717
thomasjungblut <thomas.jungblut@gmail.com>
2012-07-21 18:35:29 +0200

    static thresholding start and vectorizer

--- src/de/jungblut/ner/IterativeSimilarityAggregation.java
+++ src/de/jungblut/ner/IterativeSimilarityAggregation.java
@@ -1,13 +1,23 @@
 package de.jungblut.ner;
 
 import gnu.trove.list.array.TIntArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.collect.HashMultiset;
+
 import de.jungblut.distance.CosineDistance;
 import de.jungblut.distance.DistanceMeasurer;
 import de.jungblut.distance.SimilarityMeasurer;
 import de.jungblut.math.DoubleVector;
 import de.jungblut.math.dense.DenseDoubleMatrix;
 import de.jungblut.math.dense.DenseDoubleVector;
+import de.jungblut.math.tuple.Tuple;
 import de.jungblut.math.tuple.Tuple3;
+import de.jungblut.nlp.Tokenizer;
+import de.jungblut.nlp.Vectorizer;
 
 /**
  * Iterative similarity aggregation for named entity recognition and set
@@ -20,12 +30,17 @@ import de.jungblut.math.tuple.Tuple3;
  */
 public final class IterativeSimilarityAggregation {
 
-  private final String[] seedTokens;
   private final double alpha;
   private final SimilarityMeasurer similarityMeasurer;
+
+  private final String[] seedTokens;
   private String[] termNodes;
   private String[] contextNodes;
   private DenseDoubleMatrix weightMatrix;
+
+  private String[] dictionary;
+  private int[] seedIndices;
+  private DenseDoubleMatrix similarityMatrix;
 
   /**
    * Constructs the similarity aggregation by seed tokens to expand and a given
@@ -60,13 +75,83 @@ public final class IterativeSimilarityAggregation {
     this.weightMatrix = bipartiteGraph.getThird();
     this.alpha = alpha;
     this.similarityMeasurer = new SimilarityMeasurer(distance);
+    init();
+  }
+
+  /**
+   * Initializes the vectorized structures for algorithm use.
+   */
+  public void init() {
+    List<String[]> list = new ArrayList<>();
+    for (String s : termNodes) {
+      String[] tokenized = Tokenizer.whiteSpaceTokenize(s);
+      list.add(tokenized);
+    }
+    // we are going to add our seed tokens as well, to have them guranteed in
+    // our dictionary
+    list.add(seedTokens);
+    Tuple<HashMultiset<String>[], String[]> prepareWordCountToken = Vectorizer
+        .prepareWordCountToken(list);
+    dictionary = prepareWordCountToken.getSecond();
+    // TODO maybe play arround with bi-grams
+    List<DoubleVector> vectorized = Vectorizer.wordFrequencyVectorize(list,
+        prepareWordCountToken);
+    // now we compute the pairwise similarity matrix
+    final int n = vectorized.size();
+    similarityMatrix = new DenseDoubleMatrix(n, n);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        final double similarity = similarityMeasurer.measureSimilarity(
+            vectorized.get(i), vectorized.get(j));
+        similarityMatrix.set(i, j, similarity);
+      }
+    }
+
+    // finally we need to find our seed tokens in the dictionary
+    seedIndices = new int[seedTokens.length];
+    int i = 0;
+    for (String seed : seedTokens) {
+      int index = Arrays.binarySearch(dictionary, seed);
+      seedIndices[i++] = index;
+    }
+
   }
 
   /**
    * Starts the static thresholding algorithm and returns the expandedset of
    * newly found related tokens.
    */
-  public String[] startStaticThresholding(double similarityThreshold) {
+  public String[] startStaticThresholding(double similarityThreshold,
+      int maxIterations) {
+
+    DenseDoubleVector relevanceScore = computeRelevanceScore(seedIndices,
+        similarityMatrix);
+    int[] relevantTokens = filterRelevantItems(relevanceScore,
+        similarityThreshold);
+
+    int iteration = 0;
+    while (true) {
+
+      DenseDoubleVector similarityScore = computeRelevanceScore(relevantTokens,
+          similarityMatrix);
+      DoubleVector rankedTokens = rankScores(alpha, relevanceScore,
+          similarityScore);
+
+      /**
+       * TODO:<br/>
+       * -sort terms descending by rank<br/>
+       * -get the top K terms by rank and build the new relevant tokens<br/>
+       * <br/>
+       * -if tokens are unchanged, break<br/>
+       * -if they are not equal, merge them by score (see paper)<br/>
+       */
+
+      if (maxIterations > 0 && iteration > maxIterations) {
+        break;
+      }
+      iteration++;
+    }
+
     return seedTokens;
   }
 
