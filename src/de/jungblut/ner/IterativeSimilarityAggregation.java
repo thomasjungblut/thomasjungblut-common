99012cd20eb22b3c05705bbd6fb1e316ec7b8ad7
thomasjungblut <thomas.jungblut@gmail.com>
2012-07-21 13:59:32 +0200

    testing of the relevance and similarity calculations

--- src/de/jungblut/ner/IterativeSimilarityAggregation.java
+++ src/de/jungblut/ner/IterativeSimilarityAggregation.java
@@ -1,9 +1,12 @@
 package de.jungblut.ner;
 
+import gnu.trove.list.array.TIntArrayList;
 import de.jungblut.distance.CosineDistance;
 import de.jungblut.distance.DistanceMeasurer;
 import de.jungblut.distance.SimilarityMeasurer;
+import de.jungblut.math.DoubleVector;
 import de.jungblut.math.dense.DenseDoubleMatrix;
+import de.jungblut.math.dense.DenseDoubleVector;
 import de.jungblut.math.tuple.Tuple3;
 
 /**
@@ -56,10 +59,70 @@ public final class IterativeSimilarityAggregation {
   }
 
   /**
-   * Starts the static thresholding algorithm and returns the
+   * Starts the static thresholding algorithm and returns the expandedset of
+   * newly found related tokens.
    */
   public String[] startStaticThresholding(double similarityThreshold) {
     return seedTokens;
+  }
+
+  /**
+   * Computes the relevance for each term in U (universe of entities) to the
+   * terms in the seedset.
+   * 
+   * @param seedSet S a subset of U, this are the indices where to find the
+   *          items in the similarity matrix.
+   * @param similarityMatrix the pairwise similarity matrix of the entities.
+   * @return a vector of length of the universe of entities. Which index
+   *         encapsulates the relevance described in the paper as
+   *         S_rel(TERM_AT_INDEX_i,S)
+   */
+  static DenseDoubleVector computeRelevanceScore(int[] seedSet,
+      DenseDoubleMatrix similarityMatrix) {
+    final int termsLength = similarityMatrix.getColumnCount();
+    final DenseDoubleVector relevanceScores = new DenseDoubleVector(termsLength);
+
+    // TODO in the paper there is a multiplication with the number of terms, in
+    // the example there is nothing
+    final double constantLoss = 1.0d / seedSet.length;
+
+    for (int i = 0; i < termsLength; i++) {
+      double sum = 0.0d;
+      for (int j : seedSet) {
+        sum += similarityMatrix.get(j, i);
+      }
+      relevanceScores.set(i, constantLoss * sum);
+    }
+
+    return relevanceScores;
+  }
+
+  /**
+   * Ranks the terms at the indices by their relevance scores and the similarity
+   * scores. They are multiplied by the given alpha.
+   * 
+   * @return a vector which represents the rank of the terms.
+   */
+  static DoubleVector rankScores(double alpha,
+      DenseDoubleVector relevanceScores, DenseDoubleVector similarityScores) {
+    DoubleVector multiply = relevanceScores.multiply(alpha);
+    return similarityScores.multiply(alpha).add(multiply);
+  }
+
+  /**
+   * Returns the indices of the relevant items that are above the threshold.
+   */
+  static int[] filterRelevantItems(DenseDoubleVector relevanceScores,
+      double threshold) {
+    TIntArrayList list = new TIntArrayList();
+
+    for (int i = 0; i < relevanceScores.getLength(); i++) {
+      double val = relevanceScores.get(i);
+      if (val > threshold) {
+        list.add(i);
+      }
+    }
+    return list.toArray();
   }
 
 }
--- test/de/jungblut/ner/IterativeSimilarityAggregationTest.java
+++ test/de/jungblut/ner/IterativeSimilarityAggregationTest.java
@@ -1,14 +1,61 @@
 package de.jungblut.ner;
 
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import de.jungblut.math.DoubleVector;
+import de.jungblut.math.dense.DenseDoubleMatrix;
+import de.jungblut.math.dense.DenseDoubleVector;
+
 public final class IterativeSimilarityAggregationTest extends TestCase {
 
-  @Test
-  public void testStaticThresholding() {
+  String[] terms = new String[] { "A", "B", "C", "D", "E", "F" };
+  int[] seedSet = new int[] { 0, 1 };
+  DenseDoubleMatrix similarityMatrix = new DenseDoubleMatrix(new double[][] {
+      { 1, 0.5, 0.8, 0.7, 0.6, 0.7 }, { 0.5, 1, 0.6, 0.7, 0.7, 0.5 },
+      { 0.8, 0.6, 1, 0, 0.9, 0.3 }, { 0.7, 0.7, 0.9, 0.8, 1, 0.4 },
+      { 0.3, 0.5, 0.3, 0.5, 0.4, 1 } });
+  DenseDoubleVector relevanceScoreResult = new DenseDoubleVector(new double[] {
+      0.75, 0.75, 0.7, 0.7, 0.65, 0.6 });
 
+  DenseDoubleVector rankingResult = new DenseDoubleVector(new double[] { 0.75,
+      0.725, 0.7625, 0.625, 0.725, 0.5375 });
+
+  double threshold = 0.69;
+  double alpha = 0.5;
+
+  @Test
+  public void testRelevanceComputation() {
+    DenseDoubleVector relevanceScore = IterativeSimilarityAggregation
+        .computeRelevanceScore(seedSet, similarityMatrix);
+    double sum = relevanceScore.subtract(relevanceScoreResult).abs().sum();
+    assertTrue(sum >= 0.0d);
+    assertTrue(sum < 0.001d);
+  }
+
+  @Test
+  public void testThresholding() {
+    int[] filterRelevantItems = IterativeSimilarityAggregation
+        .filterRelevantItems(relevanceScoreResult, threshold);
+    int[] relevantItems = new int[] { 0, 1, 2, 3 };
+    assertTrue(Arrays.equals(filterRelevantItems, relevantItems));
+  }
+
+  @Test
+  public void testRanking() {
+    int[] filterRelevantItems = IterativeSimilarityAggregation
+        .filterRelevantItems(relevanceScoreResult, threshold);
+    DenseDoubleVector similarityScores = IterativeSimilarityAggregation
+        .computeRelevanceScore(filterRelevantItems, similarityMatrix);
+    DoubleVector rankScores = IterativeSimilarityAggregation.rankScores(alpha,
+        relevanceScoreResult, similarityScores);
+
+    double sum = rankScores.subtract(rankingResult).sum();
+    assertTrue(sum >= 0.0d);
+    assertTrue(sum < 0.001d);
   }
 
 }
