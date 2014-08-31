137532e3e9cdc4be14afc96434486f22f8667513
thomasjungblut <thomas.jungblut@gmail.com>
2014-08-31 01:24:19 +0100

    add L-BFGS

---
+++ test/de/jungblut/math/minimize/OWLQNTest.java
@@ -0,0 +1,38 @@
+package de.jungblut.math.minimize;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.jungblut.math.DoubleVector;
+import de.jungblut.math.dense.DenseDoubleVector;
+
+public class OWLQNTest {
+
+  @Test
+  public void testSimpleParable() {
+    int startPoint = -5;
+    // start at x=-5
+    DoubleVector start = new DenseDoubleVector(new double[] { startPoint });
+
+    // our function is f(x) = (4-x)^2+10
+    // the derivative is f'(x) = 2x-8
+    CostFunction inlineFunction = new CostFunction() {
+      @Override
+      public CostGradientTuple evaluateCost(DoubleVector input) {
+
+        double cost = Math.pow(4 - input.get(0), 2) + 10;
+        DenseDoubleVector gradient = new DenseDoubleVector(
+            new double[] { 2 * input.get(0) - 8 });
+
+        return new CostGradientTuple(cost, gradient);
+      }
+    };
+
+    DoubleVector minimizeFunction = OWLQN.minimizeFunction(inlineFunction,
+        start, 100, true);
+
+    assertEquals(4.0d, minimizeFunction.get(0), 1e-5);
+  }
+
+}
