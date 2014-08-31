1dd94d98a99a83686fdf352340a287669ffa68f5
thomasjungblut <thomas.jungblut@gmail.com>
2014-08-31 13:07:01 +0100

    add stratified splitting for train/test sets

--- src/de/jungblut/classification/eval/EvaluationSplit.java
+++ src/de/jungblut/classification/eval/EvaluationSplit.java
@@ -1,5 +1,12 @@
 package de.jungblut.classification.eval;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.google.common.base.Preconditions;
 
 import de.jungblut.datastructure.ArrayUtils;
@@ -14,6 +21,8 @@ import de.jungblut.math.DoubleVector;
  * 
  */
 public class EvaluationSplit {
+
+  private static final Log LOG = LogFactory.getLog(EvaluationSplit.class);
 
   private final DoubleVector[] trainFeatures;
   private final DoubleVector[] trainOutcome;
@@ -82,4 +91,94 @@ public class EvaluationSplit {
         testOutcome);
   }
 
+  /**
+   * Creates a new stratified evaluation split. Sampling is done based on the
+   * max index of the outcome classes (assumes one-hot encoding, or zero/one
+   * encoding for binary classes). This class does not keep the relation of the
+   * original outcome vectors to their features, thus every mutual information
+   * stored for both should be included in the feature vector.
+   * 
+   * @param features the features of your classifier.
+   * @param outcome the target variables of the classifier.
+   * @param splitFraction a value between 0f and 1f that sets the size of the
+   *          trainingset. With 1k items, a splitPercentage of 0.9f will result
+   *          in 900 items to train and 100 to evaluate.
+   * @param random true if data needs shuffling before.
+   * @return a new {@link EvaluationSplit}.
+   */
+  public static EvaluationSplit createStratified(DoubleVector[] features,
+      DoubleVector[] outcome, float splitFraction, boolean random) {
+    Preconditions.checkArgument(features.length == outcome.length,
+        "Feature vector and outcome vector must match in length!");
+    Preconditions.checkArgument(splitFraction >= 0f && splitFraction <= 1f,
+        "splitFraction must be between 0 and 1! Given: " + splitFraction);
+
+    // we group features based on their class
+
+    @SuppressWarnings("unchecked")
+    Deque<DoubleVector>[] multiQueues = new Deque[Math.max(2,
+        outcome[0].getDimension())];
+
+    DoubleVector[] sampleOutcome = new DoubleVector[multiQueues.length];
+
+    for (int i = 0; i < features.length; i++) {
+      int key = multiQueues.length == 2 ? (int) outcome[i].get(0) : outcome[i]
+          .maxIndex();
+      Deque<DoubleVector> deque = multiQueues[key];
+      if (deque == null) {
+        deque = new LinkedList<>();
+        multiQueues[key] = deque;
+      }
+      deque.addLast(features[i]);
+      sampleOutcome[key] = outcome[i];
+    }
+
+    double[] samplingProbabilities = new double[multiQueues.length];
+    for (int i = 0; i < multiQueues.length; i++) {
+      samplingProbabilities[i] = ((double) multiQueues[i].size())
+          / features.length;
+    }
+
+    LOG.info("Sampling probabilities by class: "
+        + Arrays.toString(samplingProbabilities));
+
+    final int splitSize = (int) (features.length * splitFraction);
+    DoubleVector[] trainFeatures = new DoubleVector[splitSize];
+    DoubleVector[] trainOutcomes = new DoubleVector[splitSize];
+    int offset = 0;
+    for (int s = 0; s < samplingProbabilities.length; s++) {
+      int fractionIndex = (int) (splitSize * samplingProbabilities[s]);
+      for (int i = 0; i < fractionIndex; i++) {
+        trainFeatures[offset] = multiQueues[s].poll();
+        trainOutcomes[offset] = sampleOutcome[s];
+        offset++;
+      }
+    }
+
+    // poll the rest out of the queues for the test set
+    DoubleVector[] testFeatures = new DoubleVector[features.length - splitSize];
+    DoubleVector[] testOutcomes = new DoubleVector[features.length - splitSize];
+
+    offset = 0;
+    for (int i = 0; i < multiQueues.length; i++) {
+      while (!multiQueues[i].isEmpty()) {
+        if (offset >= testFeatures.length) {
+          throw new IllegalArgumentException(
+              "Splits couldn't be fully stratified! One class must've been too small to be splittable.");
+        }
+        testFeatures[offset] = multiQueues[i].poll();
+        testOutcomes[offset] = sampleOutcome[i];
+        offset++;
+      }
+    }
+
+    if (random) {
+      // radomize both sets again
+      ArrayUtils.multiShuffle(trainFeatures, trainOutcomes);
+      ArrayUtils.multiShuffle(testFeatures, testOutcomes);
+    }
+
+    return new EvaluationSplit(trainFeatures, trainOutcomes, testFeatures,
+        testOutcomes);
+  }
 }
--- test/de/jungblut/classification/eval/EvaluationSplitTest.java
+++ test/de/jungblut/classification/eval/EvaluationSplitTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import de.jungblut.math.DoubleVector;
+import de.jungblut.math.dense.DenseDoubleVector;
 
 @RunWith(JUnit4.class)
 public class EvaluationSplitTest {
@@ -40,6 +41,84 @@ public class EvaluationSplitTest {
     assertEquals(13, split.getTestFeatures().length);
     assertEquals(87, split.getTrainOutcome().length);
     assertEquals(13, split.getTestOutcome().length);
+  }
+
+  @Test
+  public void testStratifiedSplits_50_50() {
+
+    for (int i = 0; i < outcome.length; i++) {
+      outcome[i] = new DenseDoubleVector(1);
+      outcome[i].set(0, i % 2 == 0 ? 1d : 0d);
+    }
+    EvaluationSplit split = EvaluationSplit.createStratified(feats, outcome,
+        0.5f, false);
+
+    assertEquals(50, split.getTrainFeatures().length);
+    assertEquals(50, split.getTestFeatures().length);
+    assertEquals(50, split.getTrainOutcome().length);
+    assertEquals(50, split.getTestOutcome().length);
+
+    assertEquals(25, countPositives(split.getTrainOutcome()));
+    assertEquals(25, countPositives(split.getTestOutcome()));
+  }
+
+  @Test
+  public void testStratifiedSplits_25_75() {
+
+    for (int i = 0; i < outcome.length; i++) {
+      outcome[i] = new DenseDoubleVector(1);
+      outcome[i].set(0, i < 25 ? 1d : 0d);
+    }
+    EvaluationSplit split = EvaluationSplit.createStratified(feats, outcome,
+        0.8f, false);
+
+    assertEquals(80, split.getTrainFeatures().length);
+    assertEquals(80, split.getTrainOutcome().length);
+
+    assertEquals(20, split.getTestFeatures().length);
+    assertEquals(20, split.getTestOutcome().length);
+
+    assertEquals(20, countPositives(split.getTrainOutcome()));
+    assertEquals(5, countPositives(split.getTestOutcome()));
+  }
+
+  @Test
+  public void testStratifiedSplits_1_99() {
+    for (int i = 0; i < outcome.length; i++) {
+      outcome[i] = new DenseDoubleVector(1);
+      outcome[i].set(0, i < 1 ? 1d : 0d);
+    }
+    exception.expect(IllegalArgumentException.class);
+    EvaluationSplit.createStratified(feats, outcome, 0.5f, false);
+  }
+
+  @Test
+  public void testStratifiedSplits_2_98() {
+    for (int i = 0; i < outcome.length; i++) {
+      outcome[i] = new DenseDoubleVector(1);
+      outcome[i].set(0, i < 2 ? 1d : 0d);
+    }
+    EvaluationSplit split = EvaluationSplit.createStratified(feats, outcome,
+        0.5f, false);
+
+    assertEquals(50, split.getTrainFeatures().length);
+    assertEquals(50, split.getTrainOutcome().length);
+
+    assertEquals(50, split.getTestFeatures().length);
+    assertEquals(50, split.getTestOutcome().length);
+
+    assertEquals(1, countPositives(split.getTrainOutcome()));
+    assertEquals(1, countPositives(split.getTestOutcome()));
+  }
+
+  public int countPositives(DoubleVector[] vecs) {
+    int positiveTrainClass = 0;
+    for (int i = 0; i < vecs.length; i++) {
+      if (vecs[i].get(0) == 1d) {
+        positiveTrainClass++;
+      }
+    }
+    return positiveTrainClass;
   }
 
 }
