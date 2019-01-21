540bd92564423289ef245d7a9fc15eb524cd3b23
Thomas Jungblut <thomas.jungblut@gmail.com>
2019-01-21 19:15:48 +0100

    fixing wrong list merge/sort

--- src/de/jungblut/datastructure/ListUtils.java
+++ src/de/jungblut/datastructure/ListUtils.java
@@ -1,74 +1,89 @@
 package de.jungblut.datastructure;
 
+import com.google.common.base.Preconditions;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * List util class for some fancy operations on generic lists.
- * 
+ *
  * @author thomas.jungblut
- * 
  */
 public final class ListUtils {
 
-  private ListUtils() {
-    throw new IllegalAccessError();
-  }
-
-  /**
-   * Merges two sorted segments into a single sorted list.
-   */
-  public static <K extends Comparable<K>> List<K> merge(List<K> list1,
-      List<K> list2) {
-    List<K> newList = new ArrayList<>(list1.size() + list2.size());
-
-    int offset1 = 0;
-    int offset2 = 0;
-
-    while (!list1.isEmpty() && !list2.isEmpty()) {
-      K item1 = list1.get(offset1);
-      K item2 = list2.get(offset2);
-      if (item1.compareTo(item2) <= 0) {
-        newList.add(item1);
-        list1.remove(offset1);
-      } else {
-        newList.add(item2);
-        list2.remove(offset2);
-      }
-
-      if (offset1 == list1.size()) {
-        newList.addAll(list2);
-      }
-      if (offset2 == list2.size()) {
-        newList.addAll(list1);
-      }
+    private ListUtils() {
+        throw new IllegalAccessError();
     }
 
-    return newList;
-  }
+    /**
+     * Merges two sorted segments into a single sorted list.
+     */
+    public static <K extends Comparable<K>> List<K> merge(List<K> left,
+                                                          List<K> right) {
+        checkNotNull(left, "left");
+        checkNotNull(left, "right");
 
-  /**
-   * MergeSorts the given list.
-   */
-  public static <K extends Comparable<K>> List<K> mergeSort(List<K> list) {
-    if (list.size() <= 1) {
-      return list;
-    } else {
-      int half = list.size() / 2;
-      List<K> left = mergeSort(subList(list, 0, half - 1));
-      List<K> right = mergeSort(subList(list, half, list.size() - 1));
-      return merge(left, right);
-    }
-  }
+        List<K> newList = new ArrayList<>(left.size() + right.size());
 
-  /**
-   * Sublists the given list.
-   */
-  public static <K> List<K> subList(List<K> list, int begin, int end) {
-    List<K> temp = new ArrayList<>(end - begin);
-    for (int i = begin; i <= end; i++) {
-      temp.add(list.get(i));
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            K leftElement = left.get(leftIndex);
+            K rightElement = right.get(rightIndex);
+
+            K nextElement;
+            if (leftElement.compareTo(rightElement) <= 0) {
+                nextElement = leftElement;
+                leftIndex++;
+            } else {
+                nextElement = rightElement;
+                rightIndex++;
+            }
+
+            // check whether the lists are really sorted / consistent
+            checkConsistency(newList, nextElement);
+            newList.add(nextElement);
+        }
+
+        fillRemainder(left, newList, leftIndex);
+        fillRemainder(right, newList, rightIndex);
+
+        return newList;
     }
-    return temp;
-  }
+
+    private static <K extends Comparable<K>> void fillRemainder(List<K> oldList, List<K> newList, int currentIndex) {
+        while (currentIndex < oldList.size()) {
+            K nextElement = oldList.get(currentIndex++);
+            checkConsistency(newList, nextElement);
+            newList.add(nextElement);
+        }
+    }
+
+    private static <K extends Comparable<K>> void checkConsistency(List<K> newList, K nextElement) {
+        if (!newList.isEmpty()) {
+            K lastElement = newList.get(newList.size() - 1);
+            checkArgument(nextElement.compareTo(lastElement) >= 0,
+                    "lists are not sorted, last element is not <= the current element");
+        }
+    }
+
+    /**
+     * MergeSorts the given list.
+     */
+    public static <K extends Comparable<K>> List<K> mergeSort(List<K> list) {
+        if (list.size() <= 1) {
+            return list;
+        } else {
+            int half = list.size() / 2;
+            List<K> left = mergeSort(list.subList(0, half));
+            List<K> right = mergeSort(list.subList(half, list.size()));
+            return merge(left, right);
+        }
+    }
+
 }
--- test/de/jungblut/datastructure/ListUtilsTest.java
+++ test/de/jungblut/datastructure/ListUtilsTest.java
@@ -1,46 +1,166 @@
 package de.jungblut.datastructure;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Test;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ListUtilsTest {
 
-  List<Integer> list1 = new ArrayList<>();
-  List<Integer> list2 = new ArrayList<>();
+    private void assertMergedCorrectly(List<Integer> left, List<Integer> right) {
+        // correct merge corresponds to the union of left and right and sorting
+        List<Integer> expected = new ArrayList<>(left);
+        expected.addAll(right);
+        Collections.sort(expected);
 
-  List<Integer> unsortedList = Arrays.asList(100, 12, 136, 15, 26, 723, 62,
-      6184, 8, 1, 3, 2, 72);
-
-  List<Integer> mergedList = Arrays.asList(1, 2, 2, 3, 5, 6, 7, 9, 10, 11);
-
-  {
-    list2.addAll(Arrays.asList(1, 2, 5, 7, 9, 10));
-    list1.addAll(Arrays.asList(2, 3, 6, 11));
-  }
-
-  @Test
-  public void testMerge() {
-    List<Integer> merge = ListUtils.merge(list1, list2);
-
-    for (int i = 0; i < merge.size(); i++) {
-      assertEquals(mergedList.get(i), merge.get(i));
+        List<Integer> actual = ListUtils.merge(left, right);
+        assertThat(actual, is(expected));
     }
 
-  }
-
-  @Test
-  public void TestMergeSort() {
-    List<Integer> mergeSort = ListUtils.mergeSort(unsortedList);
-    Collections.sort(unsortedList);
-
-    for (int i = 0; i < unsortedList.size(); i++) {
-      assertEquals(unsortedList.get(i), mergeSort.get(i));
+    @Test
+    public void testMergeHappyPath() {
+        assertMergedCorrectly(asList(2, 3, 6, 11), asList(1, 2, 5, 7, 9, 10));
     }
-  }
+
+    @Test
+    public void testMergeHappyPathDuplicates() {
+        assertMergedCorrectly(asList(2, 2, 3, 3, 6, 6, 11), asList(1, 1, 2, 2, 5, 5, 7, 7, 9, 10));
+    }
+
+    @Test
+    public void testMergeHappyPathDuplicatesSameElement() {
+        assertMergedCorrectly(asList(2, 2, 2, 2, 2, 2, 2), asList(2, 2, 2, 2, 2));
+    }
+
+    @Test
+    public void testMergeInterleaved() {
+        assertMergedCorrectly(asList(1, 3, 5, 7), asList(0, 2, 4, 6, 8));
+    }
+
+    @Test
+    public void testMergeNegatives() {
+        assertMergedCorrectly(asList(-10, -5, -1, 10), asList(-8, -3, 0, 5));
+    }
+
+    @Test
+    public void testMergeLeftLonger() {
+        assertMergedCorrectly(asList(0, 3, 11, 52, 93), asList(1, 2, 5, 8));
+    }
+
+    @Test
+    public void testMergeRightLonger() {
+        assertMergedCorrectly(asList(1, 2, 5, 8), asList(0, 3, 11, 52, 93));
+    }
+
+    @Test
+    public void testMergeSingleElements() {
+        assertMergedCorrectly(singletonList(1), singletonList(0));
+    }
+
+    @Test
+    public void testMergeSingleElementsReversed() {
+        assertMergedCorrectly(singletonList(0), singletonList(1));
+    }
+
+    @Test
+    public void testMergeLeftEmpty() {
+        assertMergedCorrectly(emptyList(), asList(1, 2, 5, 7, 9, 10));
+    }
+
+    @Test
+    public void testMergeRightEmpty() {
+        assertMergedCorrectly(asList(2, 3, 6, 11), emptyList());
+    }
+
+    @Test
+    public void testMergeBothEmpty() {
+        assertMergedCorrectly(Collections.<Integer>emptyList(), Collections.<Integer>emptyList());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeConsistency() {
+        ListUtils.merge(Arrays.asList(5, 3, 2, 1), Arrays.asList(1, 2, 3, 5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeConsistencySingleSwapped() {
+        ListUtils.merge(Arrays.asList(1, 2, 5, 4), Arrays.asList(1, 2, 3, 5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeConsistencySingleSwappedHungOverRight() {
+        ListUtils.merge(Arrays.asList(1, 2, 4, 5), Arrays.asList(1, 2, 5, 3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeConsistencySingleSwappedHungOverLeft() {
+        ListUtils.merge(Arrays.asList(1, 2, 5, 4), Arrays.asList(1, 2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMergeConsistencySingleSwappedReverse() {
+        ListUtils.merge(Arrays.asList(1, 2, 4, 5), Arrays.asList(1, 3, 2, 4, 5));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testMergeNullLeft() {
+        ListUtils.merge(null, Collections.<Integer>emptyList());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testMergeNullRight() {
+        ListUtils.merge(Collections.<Integer>emptyList(), null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testMergeNullBoth() {
+        ListUtils.merge(null, null);
+    }
+
+    // merge sort tests below
+
+    @Test
+    public void testMergeSortHappyPath() {
+        assertSorted(asList(5, 2, 3, 1, 200, 5102));
+    }
+
+    @Test
+    public void testMergeSortOddAmount() {
+        assertSorted(asList(13, 6, 19, 91, 52));
+    }
+
+    @Test
+    public void testMergeSortEvenAmount() {
+        assertSorted(asList(13, 6, 19, 91, 55, 36));
+    }
+
+    @Test
+    public void testMergeSortInverse() {
+        assertSorted(asList(5, 4, 3, 2, 1, 0));
+    }
+
+    @Test
+    public void testMergeSortNegatives() {
+        assertSorted(asList(-5, 4, -3, -2, 1, -1222));
+    }
+
+    @Test
+    public void testMergeSortSorted() {
+        assertSorted(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
+    private void assertSorted(List<Integer> input) {
+        List<Integer> copy = new ArrayList<>(input);
+        List<Integer> mergeSorted = ListUtils.mergeSort(new ArrayList<>(input));
+        Collections.sort(copy);
+        assertThat(mergeSorted, is(copy));
+    }
 }
